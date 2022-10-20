7a30ce6fa1a44789be889cb7655f9edfdd0f816c
unknown <sadafzillani98@gmail.com>
2022-10-20 14:31:51 +0530

    header manual routes added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -2,6 +2,11 @@ import "./Header.css";
 import Option from "../Options/Option";
 
 const Header = () => {
+  const routes = [
+    { id: 1, name: "Topics", path: "/topics" },
+    { id: 2, name: "Statistics", path: "/statistics" },
+    { id: 3, name: "Blog", path: "/blog" },
+  ];
   return (
     <nav className="navbar">
       <div className="quiz-bash">
